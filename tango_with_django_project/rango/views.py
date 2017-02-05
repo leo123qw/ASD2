7243596e232f20132a2fa8294ea7a0da9282ada5
Ji Zhang <2229755z@student.gla.ac.uk>
2017-02-05 15:44:52 +0000

    charpter8 complete newest

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,58 +5,66 @@ from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
 from django.core.urlresolvers import reverse
-def index(request):
 
-	category_list=Category.objects.order_by('-likes')[:5]
-	page_list=Page.objects.order_by('-views')[:5]
-	context_dict={"categories": category_list,"pages":page_list}
-	return render(request, 'rango/index.html', context=context_dict)
+
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {"categories": category_list, "pages": page_list}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
 def about(request):
-	print(request.method)
-	print(request.user)
-	return render(request, 'rango/about.html', {})
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
+
 
 def show_category(request, category_name_slug):
-	context_dict={}
-	try:
-		# Can we find a category name slug with the given name?
-		# If we can't, the .get() method raises a DoesNotExist exception.
+    context_dict = {}
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
-		category = Category.objects.get(slug=category_name_slug)
-        
+        category = Category.objects.get(slug=category_name_slug)
+
         # Retrieve all of the associated pages.
         # Note that filter() will return a list of page objects or an empty list
-		pages = Page.objects.filter(category=category)
+        pages = Page.objects.filter(category=category)
         # Adds our results list to the template context under name pages.
-		context_dict['pages'] = pages
-        # We also add the category object from 
+        context_dict['pages'] = pages
+        # We also add the category object from
         # the database to the context dictionary.
         # We'll use this in the template to verify that the category exists.
-		context_dict['category'] = category
-	except Category.DoesNotExist:
+        context_dict['category'] = category
+    except Category.DoesNotExist:
         # We get here if we didn't find the specified category.
-        # Don't do anything - 
+        # Don't do anything -
         # the template will display the "no category" message for us.
-		context_dict['category'] = None
-		context_dict['pages'] = None
-    
-    # Go render the response and return it to the client.
-	return render(request,'rango/category.html', context_dict)
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+        # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context_dict)
+
+
 def add_category(request):
-	form =CategoryForm()
-	if request.method=='POST':
-		form =CategoryForm(request.POST)
-		if form.is_valid():
-			form.save(commit=True)
-			return index(request)
-		else:
-			print(form.errors)
-	return render(request,'rango/add_category.html',{'form': form})
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
 def add_page(request, category_name_slug):
     try:
-        category=Category.objects.get(slug=category_name_slug)
+        category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
-        category=None
+        category = None
 
     form = PageForm()
     if request.method == 'POST':
@@ -65,11 +73,11 @@ def add_page(request, category_name_slug):
             if category:
                 page = form.save(commit=False)
                 page.category = category
-                page.views=0
+                page.views = 0
                 page.save()
                 return show_category(request, category_name_slug)
         else:
             print(form.errors)
 
-    context_dict={'form': form, 'category': category}
-    return render(request,'rango/add_page.html', context_dict)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -97,7 +97,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
+        'OPTIONS': { 'min_length': 6, }
+},
+
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
@@ -125,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+LOGIN_URL='/rango/login/'
